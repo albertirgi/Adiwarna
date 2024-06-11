b893bd7752aa3a7c5b93716e8e433fe473981a3c
albertirgi <irgialbert@gmail.com>
2024-06-11 20:28:48 +0700

    base program

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./resources/views/**/*.blade.php",
+    "./resources/views/**/*.js",
+  ],
   theme: {
     extend: {},
   },
