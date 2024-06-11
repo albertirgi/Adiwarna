35a385639f9fcbb5203d8225a823cc42ea9800c6
albertirgi <irgialbert@gmail.com>
2024-06-11 22:51:57 +0700

    video

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
 import './bootstrap';
+import '../css/video.css';
+import './video'
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,13 +1,20 @@
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    @vite('resources/js/video.js')
+
+
 </head>
+
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <video id="fullscreenVideo" class="visible" src="/assets/loading.mp4" type="video/mp4" autoplay muted
+        playsinline></video>
+    <img id="gifOverlay" class="gif-overlay" src="{{ asset('/assets/kelana-motion.gif') }}" alt="gif">
 </body>
+
 </html>
