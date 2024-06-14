8e213c4d471c2f5eaebb471e6c4bac4f1da9809d
albertirgi <irgialbert@gmail.com>
2024-06-14 16:42:44 +0700

    main video + section 1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,23 @@
-import './bootstrap';
-import '../css/style.css';
-import './video'
+import "./bootstrap";
+
+// STOP STICKY NAVBAR LOGIC
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.getElementById("navbar");
+    const mainVideo = document.getElementById("mainVideo");
+    const offset = navbar.offsetHeight;
+
+    window.addEventListener("scroll", () => {
+        const videoHeight = mainVideo.offsetHeight;
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition >= videoHeight - offset) {
+            navbar.classList.add("stop-nav");
+            navbar.style.position = "absolute";
+            navbar.style.top = `${videoHeight - offset}px`;
+        } else {
+            navbar.classList.remove("stop-nav");
+            navbar.style.position = "fixed";
+            navbar.style.top = "0";
+        }
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/views/**/*.blade.php",
-    "./resources/views/**/*.js",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'custom-gray': 'rgba(51, 51, 51, 0.8)',
-      },
+    content: ["./resources/views/**/*.blade.php", "./resources/views/**/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                "custom-gray": "rgba(51, 51, 51, 0.8)",
+                "custom-white": "rgba(246, 245, 241)",
+            },
+            fontFamily: {
+                "caslon-condensed": ['Libre Caslon Condensed', "serif"],
+                "caslon-text": ['Libre Caslon Text', "serif"],
+                "jakarta-sans": ['Plus Jakarta Sans', "sans-serif"],
+            },
+            fontSize: {
+              '10xl': '17px',
+              '11xl': '25px',
+              '12xl': '115px',
+              '13xl': '155px',
+            },
+            backgroundImage: {
+                1: "url('bg-1.svg')",
+                2: "url('bg-2.svg')",
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
