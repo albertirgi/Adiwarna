758b24132be448504a73cb089ac3c4783462adfc
albertirgi <irgialbert@gmail.com>
2024-06-16 12:55:50 +0700

    fix: navbar bug

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,28 +2,30 @@ import "./bootstrap";
 
 // STOP STICKY NAVBAR LOGIC
 document.addEventListener("DOMContentLoaded", () => {
-    const navbar = document.getElementById("navbar");
-    const mainVideo = document.getElementById("mainVideo");
-    const offset = navbar.offsetHeight;
+    setTimeout(() => {
+        const navbar = document.getElementById("navbar");
+        const mainVideo = document.getElementById("mainVideo");
+        const offset = navbar.offsetHeight;
 
-    if (navbar && mainVideo) {
-        window.addEventListener("scroll", () => {
-            const videoHeight = mainVideo.offsetHeight;
-            const scrollPosition = window.scrollY;
+        if (navbar && mainVideo) {
+            window.addEventListener("scroll", () => {
+                const videoHeight = mainVideo.offsetHeight;
+                const scrollPosition = window.scrollY;
 
-            if (scrollPosition >= videoHeight - offset) {
-                navbar.classList.add("stop-nav");
-                navbar.style.position = "absolute";
-                navbar.style.top = `${videoHeight - offset}px`;
-            } else {
-                navbar.classList.remove("stop-nav");
-                navbar.style.position = "fixed";
-                navbar.style.top = "0";
-            }
-        });
-    } else {
-        console.error("Navbar or mainVideo element not found");
-    }
+                if (scrollPosition >= videoHeight - offset) {
+                    navbar.classList.add("stop-nav");
+                    navbar.style.position = "absolute";
+                    navbar.style.top = `${videoHeight - offset}px`;
+                } else {
+                    navbar.classList.remove("stop-nav");
+                    navbar.style.position = "fixed";
+                    navbar.style.top = "0";
+                }
+            });
+        } else {
+            console.error("Navbar or mainVideo element not found");
+        }
+    }, 3200);
 });
 
 // LOADING VIDEO LOGIC
@@ -35,10 +37,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     loadingVideo.onended = function () {
         setTimeout(function () {
-            loadingVideo.style.opacity = '0';
-            mainContent.classList.remove('hidden');
+            loadingVideo.style.opacity = "0";
+            mainContent.classList.remove("hidden");
+            kelanaMotion.style.display = "block";
+            body.classList.remove("overflow-hidden");
             setTimeout(function () {
-                loadingVideo.style.zIndex = '-21';
+                loadingVideo.style.zIndex = "-21";
             }, 500);
         }, 0);
     };
