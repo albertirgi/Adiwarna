8f64fd60a4fbc2702b46ba9af80a7d3dd02517ec
albertirgi <irgialbert@gmail.com>
2024-07-17 19:55:53 +0700

    fix home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Section1 from "./Section1";
-// import Section2 from "./Section2";
+import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Section4 from "./Section4";
 import Section5 from "./Section5";
