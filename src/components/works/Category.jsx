4b54c172fd51ecff4f1c1e42794de7b7a6dd277d
albertirgi <irgialbert@gmail.com>
2024-08-05 21:51:18 +0700

    category update

--- src/components/works/Category.jsx
+++ src/components/works/Category.jsx
@@ -4,6 +4,12 @@ import FixNavbar from "../global/FixNavbar";
 import Footer from "../global/Footer";
 import Dot from "../../assets/images/dot.svg";
 
+import Thesis from "../../assets/images/thesis-image.svg"
+import Campaign from "../../assets/images/campaign-image.svg"
+import Brand from "../../assets/images/brand-image.svg"
+import Graphic from "../../assets/images/graphic-image.svg"
+import Digital from "../../assets/images/digital-image.svg"
+
 const Category = () => {
   useEffect(() => {
     const home = document.getElementById("home");
@@ -45,14 +51,14 @@ const Category = () => {
                   <Link to="/category/thesis">Thesis</Link>
                 </p>
                 <Link to="/category/thesis">
-                  <div className="bg-zinc-300 lg:h-[350px] lg:w-[460px] xl:h-[520px] xl:w-[630px]"></div>
+                  <img src={Thesis} className="lg:h-[350px] lg:w-[460px] xl:h-[480px] xl:w-[590px]" />
                 </Link>
               </div>
 
               <div className="flex self-end lg:mt-32 xl:mt-40">
                 <div className="absolute relative flex flex-col">
                   <Link to="/category/brand-and-product-innovation">
-                    <div className="bg-zinc-300 lg:h-[250px] lg:w-[540px] xl:h-[420px] xl:w-[750px]"></div>
+                    <img src={Brand} className="lg:h-[250px] lg:w-[460px] xl:h-[380px] xl:w-[710px]" />
                   </Link>
                   <p className="self-end font-caslon-condensed italic text-custom-purple lg:mt-4 lg:text-[1.4em] xl:mt-4 xl:text-[2.1em]">
                     <Link to="/category/brand-and-product-innovation">
@@ -77,13 +83,13 @@ const Category = () => {
                   </p>
                 </div>
                 <Link to="/category/digital-media">
-                  <div className="bg-zinc-300 lg:h-[350px] lg:w-[250px] xl:h-[580px] xl:w-[415px]"></div>
+                  <img src={Digital} className="lg:h-[330px] lg:w-[250px] xl:h-full xl:w-[375px]" />
                 </Link>
               </div>
 
               <div className="absolute lg:mt-[300px] xl:mt-[400px]">
                 <Link to="/category/campaign-strategy">
-                  <div className="bg-zinc-300 lg:h-[330px] lg:w-[250px] xl:h-[580px] xl:w-[415px]"></div>
+                  <img src={Campaign} className="lg:h-[330px] lg:w-[250px] xl:h-[540px] xl:w-[375px]" />
                 </Link>
                 <div className="relative flex flex-col">
                   <p className="self-end font-bold font-jakarta-sans text-custom-purple lg:text-[2em] xl:text-[3em]">
@@ -97,7 +103,7 @@ const Category = () => {
 
               <div className="absolute flex lg:mt-[800px] xl:mt-[1225px]">
                 <Link to="/category/graphic-design">
-                  <div className="bg-zinc-300 lg:h-[340px] lg:w-[340px] xl:h-[420px] xl:w-[510px]"></div>
+                  <img src={Graphic} className="lg:h-[340px] lg:w-[340px] xl:h-[380px] xl:w-full" />
                 </Link>
                 <div className="flex self-end relative flex-col lg:ml-4 xl:ml-5">
                   <p className="self-start font-bold font-jakarta-sans text-custom-purple lg:text-[2em] xl:text-[3em]">
