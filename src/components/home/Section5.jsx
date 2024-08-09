e18106af85a3f77343e820729c8ba8a4fc783034
albertirgi <irgialbert@gmail.com>
2024-08-09 17:15:51 +0700

    update carousel

--- src/components/home/Section5.jsx
+++ src/components/home/Section5.jsx
@@ -1,8 +1,13 @@
+import Carousel from "./CarouselMerch";
 import Merch1 from "../../assets/images/merch-1.svg";
-// import Merch2 from "../../assets/images/merch-2.svg";
+import Merch2 from "../../assets/images/merch-2.svg";
 import { Link } from "react-router-dom";
 
 const Section5 = () => {
+  const slides = [
+    Merch1, Merch2
+  ]
+
   return (
     <div className="bg-5 bg-cover bg-no-repeat flex flex-col w-full h-fit lg:px-14 xl:px-20">
       <div className="flex relative flex-col w-full mx-auto lg:max-w-[1024px] xl:max-w-[1440px]">
@@ -18,11 +23,15 @@ const Section5 = () => {
         </div>
         <div>
           <div className="flex tems-center justify-center">
-            <img
-              src={Merch1}
-              alt=""
-              className="w-full lg:mt-6 xl:mt-8 xl:mx-7"
-            />
+            <Carousel>
+              {slides.map((i, index) => (
+                <img
+                  key={index}
+                  src={i}
+                  className="lg:mt-6 xl:mt-8"
+                />
+              ))}
+            </Carousel>
           </div>
         </div>
         <div
