5ac7260f507a9be0b7bbee159cee473e0c1c74ff
albertirgi <irgialbert@gmail.com>
2024-08-20 20:01:03 +0700

    fixed

--- src/components/home/Section1.jsx
+++ src/components/home/Section1.jsx
@@ -4,7 +4,7 @@ const Section1 = () => {
   return (
     <div id="about" className="bg-1 bg-no-repeat bg-cover">
       <div className="flex relative flex-col w-full h-[50vh] px-3 lg:px-0 lg:h-auto mx-auto  lg:max-w-[760px]  lg:pt-20 xl:max-w-[1070px] xl:pt-32">
-        <h1 className="absolute top-4 lg:block text-custom-white font-jakarta-sans font-bold items-start text-[48px] lg:text-4lg xl:text-4xl">
+        <h1 className="absolute top-4 lg:static text-custom-white font-jakarta-sans font-bold items-start text-[48px] lg:text-4lg xl:text-4xl">
           tentang
         </h1>
 
@@ -27,13 +27,13 @@ const Section1 = () => {
                   className="w-full hover:scale-110 hover:shadow-lg transition-transform duration-300 ease-in-out"
                 />
               </figure>
-              <div className="flex flex-col absolute ml-32 mt-52 lg:ml-52 lg:pl-1 xl:ml-72 xl:pl-3">
+              <div className="flex flex-col absolute ml-32 mt-52 lg:mt-0 lg:ml-52 lg:pl-1 xl:ml-72 xl:pl-3">
                 <h2 className="text-custom-white font-caslon-condensed italic text-[52px] lg:text-5lg lg:mt-24 xl:text-5xl xl:mt-32">
                   kelana
                 </h2>
               </div>
             </div>
-            <div className="absolute bottom-32 right-4 lg:flex flex-col self-end w-[25%] lg:w-[42%]">
+            <div className="absolute bottom-32 right-4 lg:static flex flex-col self-end w-[25%] lg:w-[42%]">
               <h3 className="font-caslon-text text-custom-white text-[8px] lg:text-2lg xl:text-2xl">
                 ke·la·na
               </h3>
