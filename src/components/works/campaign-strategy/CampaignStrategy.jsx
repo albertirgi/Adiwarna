abd2a6e4d5e2000c384b59b2201e1c41bc217e3d
aldocahyadi30 <aldocahyadi28@gmail.com>
2024-08-21 21:58:13 +0700

    Update Campaign Strategy & Digital Media Index

--- src/components/works/campaign-strategy/CampaignStrategy.jsx
+++ src/components/works/campaign-strategy/CampaignStrategy.jsx
@@ -19,34 +19,36 @@ const CampaignStrategy = () => {
     <div>
       <FixNavbar />
       <div className="bg-category bg-no-repeat bg-cover h-fit lg:px-14 xl:px-20">
-        <div className="flex relative flex-col w-full mx-auto lg:max-w-[1024px] xl:max-w-[1440px]">
-          <div className="self-end lg:text-2lg lg:mt-24 xl:text-2xl xl:mt-28">
-            <Link to="/category">
-              <span className="self-end font-medium font-jakarta-sans text-custom-purple lg:mx-1 xl:mx-2">currated</span>
-              <span className="italic font-medium font-caslon-condensed text-custom-purple">works</span>
-            </Link>
+        <div className="flex relative flex-col h-screen lg:h-auto px-3 lg:px-0 w-full mx-auto lg:max-w-[1024px] xl:max-w-[1440px]">
+          <div className="flex flex-row justify-between lg:flex-col mt-16 lg:mt-0">
+            <div className="order-2 lg:order-1 self-end text-[8px] lg:text-2lg lg:mt-24 xl:text-2xl xl:mt-28">
+              <Link to="/category">
+                <span className="self-end font-medium font-jakarta-sans text-custom-purple mx-0.5 lg:mx-1 xl:mx-2">currated</span>
+                <span className="italic font-medium font-caslon-condensed text-custom-purple">works</span>
+              </Link>
+            </div>
+            <div className="order-1 lg:order-2 self-start text-[24px] lg:text-3lg xl:text-3xl">
+              <span className="self-start italic font-medium font-caslon-condensed text-custom-purple">Campaign Strategy</span>
+            </div>
           </div>
-          <div className="self-start lg:text-3lg xl:text-3xl">
-            <span className="self-start italic font-medium font-caslon-condensed text-custom-purple">Campaign Strategy</span>
-          </div>
-          <div className="flex mt-5">
+          <div className="flex mt-2 lg:mt-5">
             <article className="flex flex-col w-[33%] relative">
               <div className="flex flex-col grow text-custom-purple hover:cursor-pointer">
                 <Link to="1">
                   <img src={CS12} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={CS11} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Kalkulaku</h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Monica Yvetta Utomo</p>
+                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Kalkulaku</h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Monica Yvetta Utomo</p>
                 </Link>
               </div>
             </article>
-            <article className="flex flex-col w-[33%] lg:mx-8 xl:mx-10 relative">
+            <article className="flex flex-col w-[33%] mx-4 lg:mx-8 xl:mx-10 relative">
               <div className="flex flex-col grow text-custom-purple hover:cursor-pointer">
                 <Link to="2">
                   <img src={CS22} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={CS21} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Petallete</h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Vanessa Tan</p>
+                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Petallete</h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Vanessa Tan</p>
                 </Link>
               </div>
             </article>
@@ -55,41 +57,41 @@ const CampaignStrategy = () => {
                 <Link to="3">
                   <img src={CS32} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={CS31} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Nettika</h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Shannon Soen Ying Zhen</p>
+                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Nettika</h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Shannon Soen Ying Zhen</p>
                 </Link>
               </div>
             </article>
           </div>
-          
+
           <div className="flex mt-10 mb-40">
             <article className="flex flex-col w-[33%] relative">
               <div className="flex flex-col grow text-custom-purple hover:cursor-pointer">
                 <Link to="4">
                   <img src={CS42} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={CS41} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Harmony Inside & Out</h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Michelle Aldorino</p>
+                  <h2 className="font-jakarta-sans font-semibold lg:text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Harmony Inside & Out</h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Michelle Aldorino</p>
                 </Link>
               </div>
             </article>
-            <article className="flex flex-col w-[33%] lg:mx-8 xl:mx-10 relative">
+            <article className="flex flex-col w-[33%] mx-4 lg:mx-8 xl:mx-10 relative">
               <div className="flex flex-col grow text-custom-purple hover:cursor-pointer">
                 <Link to="5">
                   <img src={CS52} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={CS51} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">7 Broomsticks Bindery </h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Patricia Chamora</p>
+                  <h2 className="font-jakarta-sans font-semibold lg:text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">7 Broomsticks Bindery </h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Patricia Chamora</p>
                 </Link>
               </div>
             </article>
-            <article className="flex flex-col w-[33%] lg:mx-8 xl:mx-10 relative">
+            <article className="flex flex-col w-[33%] mx-4 lg:mx-8 xl:mx-10 relative">
               <div className="flex flex-col grow text-custom-purple hover:cursor-pointer">
                 <Link to="6">
                   <img src={CS62} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={CS61} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Personality Photoshoot</h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Gwyneth Charlize</p>
+                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Personality Photoshoot</h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Gwyneth Charlize</p>
                 </Link>
               </div>
             </article>
--- src/components/works/digital-media/DigitalMedia.jsx
+++ src/components/works/digital-media/DigitalMedia.jsx
@@ -18,34 +18,36 @@ const DigitalMedia = () => {
     <div>
       <FixNavbar />
       <div className="bg-category bg-no-repeat bg-cover h-fit lg:px-14 xl:px-20">
-        <div className="flex relative flex-col w-full mx-auto lg:max-w-[1024px] xl:max-w-[1440px]">
-          <div className="self-end lg:text-2lg lg:mt-24 xl:text-2xl xl:mt-28">
-            <Link to="/category">
-              <span className="self-end font-medium font-jakarta-sans text-custom-purple lg:mx-1 xl:mx-2">currated</span>
-              <span className="italic font-medium font-caslon-condensed text-custom-purple">works</span>
-            </Link>
-          </div>
-          <div className="self-start lg:text-3lg xl:text-3xl">
-            <span className="self-start italic font-medium font-caslon-condensed text-custom-purple">Digital Media</span>
-          </div>
+        <div className="flex relative flex-col h-screen lg:h-auto px-3 lg:px-0 w-full mx-auto lg:max-w-[1024px] xl:max-w-[1440px]">
+          <div className="flex flex-row justify-between lg:flex-col mt-16 lg:mt-0">
+            <div className="order-2 lg:order-1 self-end text-[8px] lg:text-2lg lg:mt-24 xl:text-2xl xl:mt-28">
+              <Link to="/category">
+                <span className="self-end font-medium font-jakarta-sans text-custom-purple mx-0.5 lg:mx-1 xl:mx-2">currated</span>
+                <span className="italic font-medium font-caslon-condensed text-custom-purple">works</span>
+              </Link>
+            </div>
+            <div className="order-1 lg:order-2 self-start text-[24px] lg:text-3lg xl:text-3xl">
+              <span className="self-start italic font-medium font-caslon-condensed text-custom-purple">Digital Media</span>
+            </div>
+          </div>  
           <div className="flex mt-5">
             <article className="flex flex-col w-[33%] relative">
               <div className="flex flex-col grow text-custom-purple hover:cursor-pointer">
                 <Link to="1">
                   <img src={DM12} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={DM11} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Pinjol Benjol</h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Katya Jocelyn</p>
+                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Pinjol Benjol</h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Katya Jocelyn</p>
                 </Link>
               </div>
             </article>
-            <article className="flex flex-col w-[33%] lg:mx-8 xl:mx-10 relative">
+            <article className="flex flex-col w-[33%] mx-4 lg:mx-8 xl:mx-10 relative">
               <div className="flex flex-col grow text-custom-purple hover:cursor-pointer">
                 <Link to="2">
                   <img src={DM22} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={DM21} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Staying Human</h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Priscillia Iva</p>
+                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Staying Human</h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Priscillia Iva</p>
                 </Link>
               </div>
             </article>
@@ -54,8 +56,8 @@ const DigitalMedia = () => {
                 <Link to="3">
                   <img src={DM32} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={DM31} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">When AI Meets Photography</h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Sharen Angelina</p>
+                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">When AI Meets Photography</h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Sharen Angelina</p>
                 </Link>
               </div>
             </article>
@@ -66,23 +68,22 @@ const DigitalMedia = () => {
                 <Link to="4">
                   <img src={DM42} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={DM41} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">At The End of Time</h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Becak Productions</p>
+                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">At The End of Time</h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Becak Productions</p>
                 </Link>
               </div>
             </article>
-            <article className="flex flex-col w-[33%] lg:mx-8 xl:mx-10 relative">
+            <article className="flex flex-col w-[33%] mx-4 lg:mx-8 xl:mx-10 relative">
               <div className="flex flex-col grow text-custom-purple hover:cursor-pointer">
                 <Link to="5">
                   <img src={DM52} className="absolute w-full opacity-0 transition-opacity duration-300 hover:opacity-100" />
                   <img src={DM51} className="w-full" />
-                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight lg:mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Absolution</h2>
-                  <p className="font-caslon-condensed font-light italic lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Ara Ara Team</p>
+                  <h2 className="font-jakarta-sans font-semibold text-justify leading-tight text-[12px] mt-2 lg:text-2lg xl:mt-3 xl:text-2xl">Absolution</h2>
+                  <p className="font-caslon-condensed font-light italic text-[8px] lg:text-1lg lg:mt-1 xl:text-1xl xl:mt-2">Ara Ara Team</p>
                 </Link>
               </div>
             </article>
             <article className="flex flex-col w-[33%] relative"></article>
-
           </div>
         </div>
       </div>
